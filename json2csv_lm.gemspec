8b5f9f8801b8b248db028a04cdda0c6f9ce383dd
Mickael Dana <mickael.dana@gmail.com>
2018-07-04 11:55:51 +0200

    First commit: exceptions handling NOT finished, NO test yet, gem NOT built

---
+++ json2csv_lm.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name = "json2csv_lm"
+  s.version = "0.0.1"
+  s.date = "2018-07-02"
+  s.summary = "convert specific json files into a csv file"
+  s.description = "A simple hello world gem"
+  s.authors     = ["Mickaël Dana"]
+  s.email       = 'mickael.dana@gmail.com'
+  s.files = ["lib/json2csv_lm.rb"]
+  s.license = 'Nonstandard'
+  s.require_paths = ["lib"]
+end
---
+++ lib/json2csv_lm.rb
@@ -0,0 +1,123 @@
+require 'json'
+require 'csv'
+
+# file not found
+# read error
+# parse error
+# content error
+# open/write error
+
+class Json2csvLm
+  # For now .convert will create or overwrite output.csv in th same dir than input_filepath
+  def self.convert(input_filepath, output_filepath = nil)
+    unless File.exist?(input_filepath)
+      puts "Error: File not found"
+      exit
+    end
+    content_hashes = parse_json_file(input_filepath)
+    content_strings_arrays = hashes_to_strings_arrays(content_hashes)
+
+    # file exist ? puts
+    # begin rescue sur storecsv     fichier bloqué accès refusé
+    storecsv("#{File.dirname(input_filepath)}/output.csv", content_strings_arrays)
+  end
+
+  private
+
+  # Parsing JSON
+  def self.parse_json_file(filepath)
+    begin
+      serialized_content = File.read(filepath)
+    rescue
+      puts "Error: readError"
+
+      exit
+    end
+
+    begin
+      content_hashes_array = JSON.parse(serialized_content)
+    rescue
+      puts "Error: parseError"
+      puts "Le fichier n'a pu être parsé, vérifié le format"
+      exit
+    end
+
+    if content_hashes_array.length == 0
+      puts "No object in the json file, operation aborted"
+      exit
+    else
+      return content_hashes_array
+    end
+  end
+
+
+  # Building the headers array
+  def self.extract_headers(hash, current_header = "")
+    res = []
+    hash.each do |k, v|
+      new_header = current_header.empty? ? k : "#{current_header}.#{k}"
+      if v.is_a?(Hash)
+        res += extract_headers(v, new_header)
+      else
+        res << new_header
+      end
+    end
+    return res
+  end
+
+  # Constructing an array of row_contents
+      # begin
+      # rescue => e
+      #   puts e.class
+      #   puts "Error: #{e.message}"
+      #   puts "All objects of the file must have the same schema "
+      # end
+  def self.hashes_to_strings_arrays(hashes_array)
+    res = []
+    headers = extract_headers(hashes_array.first)
+    res << headers
+    hashes_array.each do |hash|
+      row_content = []
+      headers.each do |header|
+        value = hash.dig(*header.split("."))
+        # array values transformation
+        if value.is_a?(Array)
+          # In case of value = [] :
+          if value.length > 0
+            row_content << value.join("|")
+          else
+            # we want nothing between the delimiters in the csv (,,) not double doublequote (,"",)
+            row_content << nil
+          end
+        else
+          row_content << value
+        end
+      end
+      res << row_content
+    end
+    return res
+  end
+
+  # Storing CSV
+  def self.storecsv(filepath, array)
+    csv_options = { col_sep: ',', force_quotes: false }
+
+    begin
+      CSV.open(filepath, 'w+', csv_options) do |csv|
+        # csv << extract_headers()
+        array.each do |row_content|
+          csv << row_content
+        end
+      end
+    rescue => e
+      puts "Error:#{e.message}"
+    end
+  end
+end
+
+# Json2csvLm.convert("home/input.json")
+Json2csvLm.convert("/home/bidjit/code/Bidjit/livementor/input.json")
+
+# h = Json2csvLm.parse_json_file("/home/bidjit/code/Bidjit/livementor/input.json").first
+# p Json2csvLm.extract_headers(h)
+# p Json2csvLm.extract_headers({ "hi" => 1, "nested" => { "hide" => "treasure"}})
